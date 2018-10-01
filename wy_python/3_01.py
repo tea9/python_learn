daebf494fe056e58af8f0cc6e8df6e04911b2f72
tea9 <shaomiaojava@gmail.com>
2018-10-01 14:47:00 +0800

    add

--- wy_python/3_01.py
+++ wy_python/3_01.py
@@ -5,5 +5,16 @@ import re
 
 url = 'https://bbs.ichunqiu.com/portal.php'
 
-r = requests.get(url=url)
+headers={
+	'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36'
+}
+
+r = requests.get(url=url,headers=headers)
 print r.status_code
+# print r.content
+
+bbs_news = re.findall('class="ui_colorG" style="color: #555555;">(.*?)</a></h3>',r.content)
+# print bbs_news
+
+for news in bbs_news:
+	print news
